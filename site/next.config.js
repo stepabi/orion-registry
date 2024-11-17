3908f066e3db4f2e1d6fdead0d3fa657a7a8dd1b
Oyedemi Abiola Stephen <32551292+stepabi@users.noreply.github.com>
2024-11-17 15:35:38 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Orionlab Kasm registry',
+    description: 'The official store for orionlab kasm.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://stepabi.github.io/orion-registry/',
+    contactUrl: 'https://stepabi.github.io/orion-registry/',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/orion-registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
